b33d339889b8c4b720f578f3132ff0a399a94581
Young Hahn <yfhahn@gmail.com>
2010-02-07 17:09:39 -0500

    Rearrange handlers so that Views is AJAX occurs after the user membership entry has been added.

--- atrium_members/atrium_members.js
+++ atrium_members/atrium_members.js
@@ -1,8 +1,61 @@
 Drupal.behaviors.atrium_members = function(context) {
   /**
+   * Retrieve the active page view if present and initialize AJAX parameters
+   * for refreshing the view via AJAX when necessary. This is fired when
+   * Drupal.attachBehaviors is called after a successful AJAX response from
+   * atrium_members.
+   */
+  if ($('.atrium-members-ajax', context).size() > 0) {
+    var form = $('.atrium-members-addform');
+    if (form) {
+      // Views AJAX prep. For the origin of much of this code, see
+      // ajax_view.js in Views.
+      if ($('input.atrium-members-addform-view', form).size() > 0) {
+        // Retrieve current page view's settings and DOM element.
+        var settings = {};
+        var view = '';
+        var identifier = $('input.atrium-members-addform-view', form).val().split(':');
+        for (var i in Drupal.settings.views.ajaxViews) {
+          if (
+            Drupal.settings.views.ajaxViews[i].view_name == identifier[0] &&
+            Drupal.settings.views.ajaxViews[i].view_display_id == identifier[1]
+          ) {
+            settings = Drupal.settings.views.ajaxViews[i];
+            view = $('.view-dom-id-' + settings.view_dom_id);
+            break;
+          }
+        }
+
+        // If there are multiple views this might've ended up showing up multiple times.
+        var ajax_path = Drupal.settings.views.ajax_path;
+        if (ajax_path.constructor.toString().indexOf("Array") != -1) {
+          ajax_path = ajax_path[0];
+        }
+
+        // Prepare viewData params.
+        var viewData = settings;
+        viewData.js = 1;
+
+        // AJAX request.
+        $.ajax({
+          url: ajax_path,
+          type: 'GET',
+          data: viewData,
+          success: function(response) {
+            // Call all callbacks.
+            if (response.__callbacks) {
+              $.each(response.__callbacks, function(i, callback) { eval(callback)(view, response); });
+            }
+          },
+          error: function(xhr) { Drupal.Views.Ajax.handleErrors(xhr, ajax_path); },
+          dataType: 'json'
+        });
+      }
+    }
+  }
+
+  /**
    * Attach to Atrium members addforms and do the following:
-   * - Retrieve the active page view if present and initialize AJAX parameters
-   *   for refreshing the view via AJAX when necessary.
    * - Event handler when hitting "enter" in the username field.
    * - Event handler when clicking the "Add" button.
    */
@@ -26,50 +79,6 @@ Drupal.behaviors.atrium_members = function(context) {
       // Clear out textfield value on submit.
       $('input.form-submit', this).mousedown(function() {
         $('input.form-text', form).val('');
-
-        // Views AJAX prep. For the origin of much of this code, see
-        // ajax_view.js in Views.
-        if ($('input.atrium-members-addform-view', form).size() > 0) {
-          // Retrieve current page view's settings and DOM element.
-          var settings = {};
-          var view = '';
-          var identifier = $('input.atrium-members-addform-view', form).val().split(':');
-          for (var i in Drupal.settings.views.ajaxViews) {
-            if (
-              Drupal.settings.views.ajaxViews[i].view_name == identifier[0] &&
-              Drupal.settings.views.ajaxViews[i].view_display_id == identifier[1]
-            ) {
-              settings = Drupal.settings.views.ajaxViews[i];
-              view = $('.view-dom-id-' + settings.view_dom_id);
-              break;
-            }
-          }
-
-          // If there are multiple views this might've ended up showing up multiple times.
-          var ajax_path = Drupal.settings.views.ajax_path;
-          if (ajax_path.constructor.toString().indexOf("Array") != -1) {
-            ajax_path = ajax_path[0];
-          }
-
-          // Prepare viewData params.
-          var viewData = settings;
-          viewData.js = 1;
-
-          // AJAX request.
-          $.ajax({
-            url: ajax_path,
-            type: 'GET',
-            data: viewData,
-            success: function(response) {
-              // Call all callbacks.
-              if (response.__callbacks) {
-                $.each(response.__callbacks, function(i, callback) { eval(callback)(view, response); });
-              }
-            },
-            error: function(xhr) { Drupal.Views.Ajax.handleErrors(xhr, ajax_path); },
-            dataType: 'json'
-          });
-        }
       });
     });
 };
