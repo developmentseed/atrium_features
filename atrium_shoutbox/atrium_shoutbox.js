e238e2d919a6bd7bc61579057fb09393212d2e9a
Young Hahn <yfhahn@gmail.com>
2010-07-26 13:28:02 -0400

    Remove messaging_shoutbox, merge functionality into atrium_shoutbox

--- atrium_shoutbox/atrium_shoutbox.js
+++ atrium_shoutbox/atrium_shoutbox.js
@@ -1,12 +1,27 @@
 /**
  * Drupal behaviors implementation for Atrium Shoutbox.
- *
- * Adds a handler to the Atrium Shoutbox launcher link that sets a cookie
- * with the current pageload's server timestamp when clicked. The cookie
- * is used server-side for the non-critical task of determining how many
- * shouts have not been viewed by the current user.
  */
 Drupal.behaviors.atrium_shoutbox = function(context) {
+  /**
+   * Mark shoutboxes as "submitting" while AHAH is processing. After submission
+   * clear the shoutbox (attachBehaviors is called by ahah.js).
+   */
+  $('form.shoutform.submitted textarea').removeClass('submitted').val('');
+  $('form.shoutform:not(.atrium-shoutbox-processed)')
+    .addClass('atrium-shoutbox-processed')
+    .each(function() {
+      var form = $(this);
+      $('input.form-submit', this).mousedown(function() {
+        form.addClass('submitted');
+      });
+    });
+
+  /**
+   * Adds a handler to the Atrium Shoutbox launcher link that sets a cookie
+   * with the current pageload's server timestamp when clicked. The cookie
+   * is used server-side for the non-critical task of determining how many
+   * shouts have not been viewed by the current user.
+   */
   if (jQuery.cookie && Drupal.settings.atrium_shoutbox.timestamp) {
     $('#atrium-shoutbox-launcher:not(.atrium-shoutbox-processed)')
       .addClass('atrium-shoutbox-processed')
